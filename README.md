4b052c945a25458850303176608764d1ead792a0
白彩恋 <shiro@oom-wg.dev>
2025-10-28 01:29:36 +0800

    大部分内容

---
+++ README.md
@@ -0,0 +1,1 @@
+# [《寻找伪人》游戏攻略文档](https://non-human.game.oom-wg.dev)
---
+++ static/umeng.js
@@ -0,0 +1,13 @@
+;(function (w, d, s, q, i) {
+	w[q] = w[q] || []
+	const f = d.getElementsByTagName(s)[0],
+		j = d.createElement(s)
+	j.async = true
+	j.id = 'beacon-aplus'
+	j.src = 'https://d.alicdn.com/alilog/mlog/aplus/' + i + '.js'
+	f.parentNode.insertBefore(j, f)
+})(window, document, 'script', 'aplus_queue', '203467608')
+aplus_queue.push({
+	action: 'aplus.setMetaInfo',
+	arguments: ['appKey', '68ff093e8560e34872d6c1c9']
+})
